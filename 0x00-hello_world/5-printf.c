af6933c705d7612dbfa958956855f1661e952835
iamsancit <ajayioluwasanmi25@gmail.com>
2023-03-19 18:12:16 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function that prints
+ *
+ * Return: 0 when it is done
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
